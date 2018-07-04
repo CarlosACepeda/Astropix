b6e3a590c8b6b7be89af42b829477221f19802f1
Carlos Cepeda <carlosalt5126@hotmail.es>
2018-07-04 16:01:55 -0500

    Agregar archivos de proyecto.

---
+++ Astropix/Services/AstropicRetrieverService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace Astropix.Services
+{
+    [Service(Label ="Image of the Day retriever service")]
+    class AstropicRetrieverService : Service
+    {
+        private readonly string url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            RetrieveTheImageOfTheDay();
+            return base.OnStartCommand(intent, flags, startId);
+        }
+        /// <summary>
+        /// This method retrieves the actual image od the day from the NASA Apod API.
+        /// </summary>
+        private async void RetrieveTheImageOfTheDay()
+        {
+            var client = new HttpClient();
+            var result= await client.GetStringAsync(url);
+            var post = JsonConvert.DeserializeObject<ImageOfTheDay>(result);
+        }
+    }
+}
---
+++ Astropix/Services/ImageOfTheDay.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Astropix.Services
+{
+    /// <summary>
+    /// A class used to build a 'Image of the day' that the Activity will use to show Information about this image and the image itself
+    /// </summary>
+    class ImageOfTheDay
+    {
+        /// <summary>
+        /// The date of the picture
+        /// </summary>
+        [XmlElement(ElementName ="date")]
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// The title of the picture
+        /// </summary>
+        [XmlElement(ElementName ="title")]
+        public string Title { get; set; }
+        /// <summary>
+        /// Copyright of the picture, can be null.
+        /// </summary>
+        [XmlElement(ElementName = "copyright")]
+        public string Copyright { get; set; }
+        /// <summary>
+        /// Description of the Image
+        /// </summary>
+        [XmlElement(ElementName = "explanation")]
+        public string Explanation { get; set; }
+
+        ///// <summary>
+        ///// A picture in standard quality 1024*768
+        ///// </summary>
+        //public string Image { get; set; }
+        ///// <summary>
+        ///// A picture in 4k quality.
+        ///// </summary>
+        //public string ImageHd { get; set; }
+
+    }
+}
